2d0b610d4fd5f0c541038e7183f81b2584e3c026
noidname01 <tim983649@gmail.com>
2022-01-19 02:08:36 +0800

    ver 1 toplife

--- filetoplife.py
+++ filetoplife.py
@@ -1,23 +1,10 @@
-#!/usr/bin/python
-# @lint-avoid-python-3-compatibility-imports
-#
-# filetop  file reads and writes by process.
-#          For Linux, uses BCC, eBPF.
-#
-# USAGE: filetop.py [-h] [-C] [-r MAXROWS] [interval] [count]
-#
-# This uses in-kernel eBPF maps to store per process summaries for efficiency.
-#
-# Copyright 2016 Netflix, Inc.
-# Licensed under the Apache License, Version 2.0 (the "License")
-#
-# 06-Feb-2016   Brendan Gregg   Created this.
-
 from __future__ import print_function
 from bcc import BPF
 from time import sleep, strftime
 import argparse
 from subprocess import call
+from datetime import datetime
+
 
 # arguments
 examples = """examples:
@@ -31,15 +18,10 @@ parser = argparse.ArgumentParser(
     description="File reads and writes by process",
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=examples)
-parser.add_argument("-a", "--all-files", action="store_true",
-    help="include non-regular file types (sockets, FIFOs, etc)")
 parser.add_argument("-C", "--noclear", action="store_true",
     help="don't clear the screen")
 parser.add_argument("-r", "--maxrows", default=20,
     help="maximum rows to print, default 20")
-parser.add_argument("-s", "--sort", default="all",
-    choices=["all", "reads", "writes", "rbytes", "wbytes"],
-    help="sort column, default all")
 parser.add_argument("-p", "--pid", type=int, metavar="PID", dest="tgid",
     help="trace this PID only")
 parser.add_argument("interval", nargs="?", default=1,
@@ -55,13 +37,11 @@ maxrows = int(args.maxrows)
 clear = not int(args.noclear)
 debug = 0
 
-# linux stats
-loadavg = "/proc/loadavg"
-
-# define BPF program
 bpf_text = """
 #include <uapi/linux/ptrace.h>
 #include <linux/blkdev.h>
+#include <linux/fs.h>
+#include <linux/sched.h>
 // the key for the output summary
 struct info_t {
     unsigned long inode;
@@ -75,12 +55,10 @@ struct info_t {
 };
 // the value of the output summary
 struct val_t {
-    u64 reads;
-    u64 writes;
-    u64 rbytes;
-    u64 wbytes;
+    u64 time;
 };
 BPF_HASH(counts, struct info_t, struct val_t);
+
 static int do_entry(struct pt_regs *ctx, struct file *file,
     char __user *buf, size_t count, int is_read)
 {
@@ -92,7 +70,7 @@ static int do_entry(struct pt_regs *ctx, struct file *file,
     struct dentry *de = file->f_path.dentry;
     int mode = file->f_inode->i_mode;
     struct qstr d_name = de->d_name;
-    if (d_name.len == 0 || TYPE_FILTER)
+    if (d_name.len == 0 || !S_ISREG(mode))
         return 0;
     // store counts and sizes by pid & file
     struct info_t info = {
@@ -103,65 +81,72 @@ static int do_entry(struct pt_regs *ctx, struct file *file,
     bpf_get_current_comm(&info.comm, sizeof(info.comm));
     info.name_len = d_name.len;
     bpf_probe_read_kernel(&info.name, sizeof(info.name), d_name.name);
-    if (S_ISREG(mode)) {
-        info.type = 'R';
-    } else if (S_ISSOCK(mode)) {
-        info.type = 'S';
-    } else {
-        info.type = 'O';
-    }
+    info.type = 'W'
+
     struct val_t *valp, zero = {};
     valp = counts.lookup_or_try_init(&info, &zero);
-    if (valp) {
-        if (is_read) {
-            valp->reads++;
-            valp->rbytes += count;
-        } else {
-            valp->writes++;
-            valp->wbytes += count;
-        }
-    }
+    valp->time += bpf_ktime_get_ns();
+
+
     return 0;
-}
-int trace_read_entry(struct pt_regs *ctx, struct file *file,
-    char __user *buf, size_t count)
-{
-    return do_entry(ctx, file, buf, count, 1);
 }
 int trace_write_entry(struct pt_regs *ctx, struct file *file,
     char __user *buf, size_t count)
 {
     return do_entry(ctx, file, buf, count, 0);
 }
+
+// trace file deletion and output details
+int trace_unlink(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
+{
+    u32 tgid = bpf_get_current_pid_tgid() >> 32;
+    if (TGID_FILTER)
+            return 0;
+
+    struct qstr d_name = dentry->d_name;
+    if (d_name.len == 0)
+        return 0;
+
+    // store counts and sizes by pid & file
+    struct info_t info = {
+        .pid = pid,
+    };
+
+    bpf_get_current_comm(&info.comm, sizeof(info.comm));
+    info.name_len = d_name.len;
+    bpf_probe_read_kernel(&info.name, sizeof(info.name), d_name.name);
+    info.type = 'D'
+
+    struct val_t *valp, zero = {};
+    valp = counts.lookup_or_try_init(&info, &zero);
+    valp->time += bpf_ktime_get_ns();
+
+    return 0;
+}
 """
+
 if args.tgid:
     bpf_text = bpf_text.replace('TGID_FILTER', 'tgid != %d' % args.tgid)
 else:
     bpf_text = bpf_text.replace('TGID_FILTER', '0')
-if args.all_files:
-    bpf_text = bpf_text.replace('TYPE_FILTER', '0')
-else:
-    bpf_text = bpf_text.replace('TYPE_FILTER', '!S_ISREG(mode)')
 
 if debug or args.ebpf:
     print(bpf_text)
     if args.ebpf:
         exit()
 
+
 # initialize BPF
 b = BPF(text=bpf_text)
-# b.attach_kprobe(event="vfs_read", fn_name="trace_read_entry")
 b.attach_kprobe(event="vfs_write", fn_name="trace_write_entry")
+b.attach_kprobe(event="vfs_unlink", fn_name="trace_unlink")
 
 DNAME_INLINE_LEN = 32  # linux/dcache.h
 
 print('Tracing... Output every %d secs. Hit Ctrl-C to end' % interval)
 
 def sort_fn(counts):
-    if args.sort == "all":
-        return (counts[1].rbytes + counts[1].wbytes + counts[1].reads + counts[1].writes)
-    else:
-        return getattr(counts[1], args.sort)
+    return (counts[1].time)
 
 # output
 exiting = 0
@@ -176,10 +161,7 @@ while 1:
         call("clear")
     else:
         print()
-    with open(loadavg) as stats:
-        print("%-8s loadavg: %s" % (strftime("%H:%M:%S"), stats.read()))
-    print("%-7s %-16s %-6s %-6s %-7s %-7s %1s %s" % ("TID", "COMM",
-        "READS", "WRITES", "R_Kb", "W_Kb", "T", "FILE"))
+    print("%s %-7s %-16s %1s %s" % ("TIME" ,"TID", "COMM", "T", "FILE"))
 
     # by-TID output
     counts = b.get_table("counts")
@@ -191,10 +173,13 @@ while 1:
             name = name[:-3] + "..."
 
         # print line
-        print("%-7d %-16s %-6d %-6d %-7d %-7d %1s %s" % (k.pid,
-            k.comm.decode('utf-8', 'replace'), v.reads, v.writes,
-            v.rbytes / 1024, v.wbytes / 1024,
-            k.type.decode('utf-8', 'replace'), name))
+        print("%s %-7s %-16s %1s %s" % (
+            datetime.fromtimestamp(v.time // 1000000000).strftime('%Y-%m-%d %H:%M:%S'),   
+            k.pid,
+            k.comm.decode('utf-8', 'replace'),
+            k.type.decode('utf-8', 'replace'), 
+            name
+        ))
 
         line += 1
         if line >= maxrows:
