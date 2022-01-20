3cabfb1141ea1ed80eb6f0d6ee795d30fab84872
billy chia <billy.12543@gmail.com>
2022-01-21 01:42:09 +0800

    add startup and end

--- filetoplife.py
+++ filetoplife.py
@@ -43,7 +43,33 @@ maxrows = int(args.maxrows)
 clear = not int(args.noclear)
 debug = bool(int(args.debug))
 
-bpf_text = """
+print('This program will create a snapshot right now.')
+if input("Do you want to continue? [y/n]").strip().lower() == "n":
+    exit()
+
+snapshot = LVM()
+print('--- Current existing LVMs ---\n')
+for i, lv in enumerate(snapshot.lvs):
+    print(f'Index: {i} , Name: {lv["n"]}')
+
+print('')
+sel = int(input('Which LVM do you want to protect? [Index num]'))
+avaliable_size = 0
+for pv in snapshot.pvs:
+    if pv['vg'] == snapshot.lvs[sel]['vg']:
+        avaliable_size += pv['PEsize']*(pv['freePE']-3)
+
+print(f'Current avaliable size for snapshot is {avaliable_size} MB')
+
+size = int(input('\nHow much size do you want to allocate for snapshot? [MB]'))
+
+
+snapshot.createSnapshot(sel,size)
+
+
+
+try:
+    bpf_text = """
 #include <uapi/linux/ptrace.h>
 #include <linux/fs.h>
 #include <linux/sched.h>
@@ -139,137 +165,133 @@ int trace_unlink(struct pt_regs *ctx, struct inode *dir, struct dentry *dentry)
     }
 }
 """
+    if args.tgid:
+        bpf_text = bpf_text.replace('TGID_FILTER', 'tgid != %d' % args.tgid)
+    else:
+        bpf_text = bpf_text.replace('TGID_FILTER', '0')
 
-if args.tgid:
-    bpf_text = bpf_text.replace('TGID_FILTER', 'tgid != %d' % args.tgid)
-else:
-    bpf_text = bpf_text.replace('TGID_FILTER', '0')
+    # initialize BPF
+    b = BPF(text=bpf_text, )
+    b.attach_kprobe(event="vfs_write", fn_name="trace_write_entry")
+    b.attach_kprobe(event="vfs_unlink", fn_name="trace_unlink")
 
-# initialize BPF
-b = BPF(text=bpf_text, )
-b.attach_kprobe(event="vfs_write", fn_name="trace_write_entry")
-b.attach_kprobe(event="vfs_unlink", fn_name="trace_unlink")
+    command_to_comm = {}
+    comm_to_command = {}
+    comm_filename_set = set()
+    candidators_info = {}
 
-snapshot = LVM()
-# snapshot.createSnapshot(0, size)
-print("Mounting...")
-mount_path = snapshot.mountSnapshot(0)
+    # the path we trust
+    whilelist = ["/usr", "/lib", "/sbin"]
 
-command_to_comm = {}
-comm_to_command = {}
-comm_filename_set = set()
-candidators_info = {}
+    def print_event(cpu, data, size):
+        event = b["events"].event(data)
+        name = event.name.decode('utf-8', 'replace')
+        comm = event.comm.decode('utf-8', 'replace')
 
-# the path we trust
-whilelist = ["/usr", "/lib", "/sbin"]
+        # run 'ps aux -L | grep pid' to get the executor
+        p = subprocess.Popen(f'ps aux -L | grep " {str(event.pid)} "',shell=True, stdout=subprocess.PIPE)
+        command_list = list(map(lambda x:" ".join(x.split("   ")[-1].split(" ")[1:]).strip() ,p.stdout.read().decode('utf-8').splitlines()))
+        command_list = list(filter(lambda x: x.replace("grep", "") == x, command_list))
 
-def print_event(cpu, data, size):
-    event = b["events"].event(data)
-    name = event.name.decode('utf-8', 'replace')
-    comm = event.comm.decode('utf-8', 'replace')
-
-    # run 'ps aux -L | grep pid' to get the executor
-    p = subprocess.Popen(f'ps aux -L | grep " {str(event.pid)} "',shell=True, stdout=subprocess.PIPE)
-    command_list = list(map(lambda x:" ".join(x.split("   ")[-1].split(" ")[1:]).strip() ,p.stdout.read().decode('utf-8').splitlines()))
-    command_list = list(filter(lambda x: x.replace("grep", "") == x, command_list))
-
-    try:
-        command = command_list[0].split(" ")[0]
-        # construct comm to command dict, to record the mapping relation ship
-        if command not in command_to_comm:
-            command_to_comm[command] = set()
-        
-        command_to_comm[command].add(comm)
-        comm_to_command[comm] = command
-
-    except:
-        # process has been stopped
         try:
-            # if we have seen this comm before, get its command
-            command = comm_to_command[comm]
+            command = command_list[0].split(" ")[0]
+            # construct comm to command dict, to record the mapping relation ship
+            if command not in command_to_comm:
+                command_to_comm[command] = set()
+            
+            command_to_comm[command].add(comm)
+            comm_to_command[comm] = command
+
         except:
-            # we didn't see it before
-            command = "This comm comes from a stopped process"
+            # process has been stopped
+            try:
+                # if we have seen this comm before, get its command
+                command = comm_to_command[comm]
+            except:
+                # we didn't see it before
+                command = "This comm comes from a stopped process"
 
-    finally:
-        # if the executor doesn't in the whitelist, record it to be processed
-        has_whitepath = list(filter(lambda x: command.replace(x, "") != command, whilelist))
-        if debug and len(has_whitepath) == 0:
-            if (comm, name) not in comm_filename_set:
-                print("%-10d %-7s %-16s %4s %-64s" % (
-                            event.order,
-                            event.pid,
-                            comm,
-                            event.type.decode('utf-8', 'replace'), 
-                            name,
-                ))
-                comm_filename_set.add((comm, name))
-                candidators_info[str(event.order)] = {
-                    "pid":event.pid,
-                    "comm":comm,
-                    "type": event.type.decode('utf-8', 'replace'), 
-                    "filename": name,
-                }
+        finally:
+            # if the executor doesn't in the whitelist, record it to be processed
+            has_whitepath = list(filter(lambda x: command.replace(x, "") != command, whilelist))
+            if debug and len(has_whitepath) == 0:
+                if (comm, name) not in comm_filename_set:
+                    print("%-10d %-7s %-16s %4s %-64s" % (
+                                event.order,
+                                event.pid,
+                                comm,
+                                event.type.decode('utf-8', 'replace'), 
+                                name,
+                    ))
+                    comm_filename_set.add((comm, name))
+                    candidators_info[str(event.order)] = {
+                        "pid":event.pid,
+                        "comm":comm,
+                        "type": event.type.decode('utf-8', 'replace'), 
+                        "filename": name,
+                    }
+         
+    b["events"].open_perf_buffer(print_event)
 
-        
-b["events"].open_perf_buffer(print_event)
+    print('The program now is Tracking your FileSystem and able to recover your filesystem.')
+    print("Press 'Ctrl + C' to stop Tracking and go into recovery mode")
 
-# header
-if debug:
-    print('Tracing... Output every %d secs. Hit Ctrl-C to end' % interval)
-    print("%-10s %-7s %-16s %4s %-64s" % ("ORDER" ,"TID", "COMM", "TYPE", "FILE"))   
-else:
-    print('Scanning... ')
+    if debug:
+        print('Tracing... Output every %d secs. Hit Ctrl-C to end' % interval)
+        print("%-10s %-7s %-16s %4s %-64s" % ("ORDER" ,"TID", "COMM", "TYPE", "FILE"))   
+    else:
+        print('Scanning... ')
 
-# run until the duration over
-start_time = time.time()
-while time.time() - start_time < duration:
-    try:
+    # run until the duration over
+    start_time = time.time()
+    while time.time() - start_time < duration:
         b.perf_buffer_poll()
-    except KeyboardInterrupt:
-        print("Bye!")
-        exit()
-
-file_recoverer = FileRe('/', mount_path)
 
 
-print("%-5s %-7s %-16s %4s %-64s" % ("ORDER" ,"TID", "COMM", "TYPE", "FILE"))   
+except KeyboardInterrupt:
+    print("\nNow you are in the recovery mode.")
+    print("Mounting...")
+    mount_path = snapshot.mountSnapshot(sel)
+    file_recoverer = FileRe('/', mount_path)
+# header
 
-count = 0
-full_path_filenames = []
-display = []
-for order, info in candidators_info.items():
-    all_possibility = file_recoverer.query(info["filename"])
-    for possibility in all_possibility:
-        t = ("%-7s %-16s %4s %-64s" % (
-                            info["pid"],
-                            info["comm"],
-                            info["type"], 
-                            possibility["p"],
-            ))
-        print("%5d " % (count) + t)
-        full_path_filenames.append(possibility["p"])
-        display.append(t)
-        count += 1
+    print("%-5s %-7s %-16s %4s %-64s" % ("ORDER" ,"TID", "COMM", "TYPE", "FILE"))   
 
-while len(full_path_filenames) > 0 and input("Continue to Recover?[y/n]").strip().lower() != "n":
-    
-    choosed = list(map(lambda x:int(x), input("Choose the file you want to recover:" ).split(" ")))
-    to_be_recovered = list(map(lambda x : full_path_filenames[int(x)],choosed))
+    count = 0
+    full_path_filenames = []
+    display = []
+    for order, info in candidators_info.items():
+        all_possibility = file_recoverer.query(info["filename"])
+        for possibility in all_possibility:
+            t = ("%-7s %-16s %4s %-64s" % (
+                                info["pid"],
+                                info["comm"],
+                                info["type"], 
+                                possibility["p"],
+                ))
+            print("%5d " % (count) + t)
+            full_path_filenames.append(possibility["p"])
+            display.append(t)
+            count += 1
 
-    for i in choosed:
-        full_path_filenames[i] = ""
-        display[i] = ""        
-    full_path_filenames = list(filter(lambda x: x!="", full_path_filenames))
-    display = list(filter(lambda x:x!="", display))
+    while len(full_path_filenames) > 0 and input("Continue to Recover?[y/n]").strip().lower() != "n":
+        
+        choosed = list(map(lambda x:int(x), input("Choose the file you want to recover:" ).split(" ")))
+        to_be_recovered = list(map(lambda x : full_path_filenames[int(x)],choosed))
 
-    for i, t in enumerate(display):
-        print("%5d " % (i) + t)
+        for i in choosed:
+            full_path_filenames[i] = ""
+            display[i] = ""        
+        full_path_filenames = list(filter(lambda x: x!="", full_path_filenames))
+        display = list(filter(lambda x:x!="", display))
 
-    file_recoverer.recovery(to_be_recovered)
+        for i, t in enumerate(display):
+            print("%5d " % (i) + t)
 
-    print("Congraturation! Recovery Success.")
+        file_recoverer.recovery(to_be_recovered)
 
+        print("Congraturation! Recovery Success.")
+
+    snapshot.unmountSnapshot(sel)
+    snapshot.removeSnapshot(sel)
 print("Bye!")
-# snapshot.unmountSnapshot(0)
-# snapshot.removeSnapshot(0)
